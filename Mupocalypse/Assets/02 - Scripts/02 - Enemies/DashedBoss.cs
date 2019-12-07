2feb4ea13cd6605290b8bad9426c2ec3d3b44eda
hl <hel.lud@gmx.de>
2019-12-07 15:53:34 +0100

    Added basic snap functionality

---
+++ Mupocalypse/Assets/02 - Scripts/02 - Enemies/DashedBoss.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashedBoss : MonoBehaviour, IDamageable
+{
+    void Start()
+    {
+        
+    }
+
+    public void GetDamage()
+    {
+        
+    }
+}
---
+++ Mupocalypse/Assets/02 - Scripts/02 - Enemies/DashedEnemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashedEnemy : MonoBehaviour, IDamageable
+{
+    [SerializeField]
+    private float extraLength;
+    [SerializeField]
+    private float withdrawSpeed;
+
+    private float idleLowness;
+    private float extendedLowness;
+    private int lives;
+
+    public bool doSnap;
+
+    void Start()
+    {
+        idleLowness = transform.position.y;
+        extendedLowness = idleLowness - extraLength;
+        lives = 2;
+
+        doSnap = false;
+    }
+
+    void Update()
+    {
+        if (doSnap)
+        {
+            StartCoroutine(Snap());
+            doSnap = false;
+        }
+    }
+    
+    public void GetDamage()
+    {
+        lives--;
+        if (lives == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    IEnumerator Snap()
+    {
+        // Snap
+        while (transform.position.y > extendedLowness)
+        {
+            yield return new WaitForFixedUpdate();
+            float y = transform.position.y - withdrawSpeed * Time.deltaTime * 8;
+            y = y > extendedLowness ? y : extendedLowness;
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        }
+        yield return new WaitForSeconds(1);
+        // Return to idle
+        while (transform.position.y < idleLowness)
+        {
+            yield return new WaitForFixedUpdate();
+            float y = transform.position.y + withdrawSpeed * Time.deltaTime;
+            y = y < idleLowness ? y : idleLowness;
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        }
+    }
+}
