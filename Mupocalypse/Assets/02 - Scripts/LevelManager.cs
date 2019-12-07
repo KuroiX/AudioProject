d5caa0b47dc6060017618cb299c314632d6d67cc
Carolina <carolina.kawabata@gmail.com>
2019-12-07 05:10:24 +0100

    fade animation

---
+++ Mupocalypse/Assets/02 - Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : Singleton<LevelManager>
+{
+
+
+    private int roomID;
+    public Animator fade;
+    public void LoadScene(int roomID)
+    {
+        FadeOut();
+        this.roomID = roomID;
+        SceneManager.LoadScene(roomID);
+        
+        FadeIn();
+       
+    }
+
+    public void FadeIn()
+    {
+      fade.SetTrigger("FadeIn");
+        
+        
+    }
+
+    public void FadeOut()
+    {
+        fade.SetTrigger("FadeOut");
+    }
+
+    
+}
