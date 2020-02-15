6ae3ddccd0e713f4e1bc5f03d7fc1a5f9e0b65db
KuroiX <julian.geheeb@gmx.de>
2020-02-15 22:08:03 +0100

    (hopefully) final bugfixes

--- Mupocalypse/Assets/AudioManager.cs
+++ Mupocalypse/Assets/AudioManager.cs
@@ -243,41 +243,5 @@ public class AudioManager : Singleton<AudioManager>
         SetMaster(currentVolume);
         PlayClip(clip);
     }
-
-
-    /*IEnumerator FadeMusic (float duration, float value)
-    {
-        float currentTime = 0;
-        float currentVolume = musicVolume;
-        float difference = currentVolume - value;
-        
-        while (currentTime < duration)
-        {
-            SetMusic(currentVolume - (currentTime/duration * difference));
-            currentTime += Time.deltaTime;
-            yield return null;
-        }
-
-        SetMusic(value);
-    }*/
     
-    /*
-    public static IEnumerator StartFade(AudioMixer audioMixer, string exposedParam, float duration, float targetVolume)
-    {
-        float currentTime = 0;
-        float currentVol;
-        audioMixer.GetFloat(exposedParam, out currentVol);
-        currentVol = Mathf.Pow(10, currentVol / 20);
-        float targetValue = Mathf.Clamp(targetVolume, 0.0001f, 1);
-
-        while (currentTime < duration)
-        {
-            currentTime += Time.deltaTime;
-            float newVol = Mathf.Lerp(currentVol, targetValue, currentTime / duration);
-            audioMixer.SetFloat(exposedParam, Mathf.Log10(newVol) * 20);
-            yield return null;
-        }
-        yield break;
-    }
-    */
 }
